ba747fea63e89e0338a9725ef4fc3c4cbf3f7d9c
alin_alok <alokmahtab88@gmail.com>
2022-08-08 21:20:09 +0600

    the first commit from mybranch

---
+++ diff_con.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int time;
+    cout<<"enter time"<<endl;
+    cin>>time;
+    if(time < 10){
+        cout<<"goood"<<endl;
+
+    }
+    else{
+        cout<<"bad"<<endl;
+
+    }
+    return 0;
+}
