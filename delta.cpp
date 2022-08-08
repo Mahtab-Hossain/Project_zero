80bf86a106e384f7e317fad2db204ecbb1c4a81e
alin_alok <alokmahtab88@gmail.com>
2022-08-08 23:10:47 +0600

    make it more funnier if you guys could

--- delta.cpp
+++ delta.cpp
@@ -1,1 +1,22 @@
 #include <iostream>
+using namespace std;
+
+int main(){
+    int x,y,z;
+    cout<<"enter the value of x: ";
+    cin>>x;
+    switch (x>= 5 )
+    {
+    case 1:
+        cout<<"go to hell musa the pasamoda";
+        break;
+    case 2:
+        cout<<"go to the small hell sadi";
+        break;
+    case 3:
+        cout<<"morshed the real introvert";
+    default:
+        cout<<"everything is okay";
+        break;
+    }
+}
